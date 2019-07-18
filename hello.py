abe5d22a4b9dc088e2f1f80d5bd4d7506c0b765b
joshuayuvan <joshuaedwinragul@gmail.com>
2019-07-18 12:11:06 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+a = int(input('Type a number: '))
+if a >= 1:
+  print('It is a positive number')
+elif a<= -1:
+  print('It is a negative number')
+else:
+  print('It is neither positive nor negative (zero)')
